6c7d5848d6030c49a115135442b0853c89c44d64
Sofiyakhan16 <sofiyakhanamjms@gmail.com>
2024-11-08 11:21:14 +0530

    Token issue and activation validation  in login page is done, passing the CustomerTypeId in register

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,29 +1,5 @@
-// import axios from 'axios';
-
-// axios.defaults.baseURL = 'http://ec2-100-29-38-82.compute-1.amazonaws.com:5000/';
-
-// axios.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-
-//     if (token && !config.url.includes('/api/Customer/Login') && !config.url.includes('/api/Customer/AdminLogin')) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default axios;
-
-
-
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://ec2-100-29-38-82.compute-1.amazonaws.com:5000/';
 axios.defaults.baseURL = 'https://ec2-100-29-38-82.compute-1.amazonaws.com';
 
 // Set up request interceptor to add Authorization header
@@ -51,13 +27,22 @@ axios.interceptors.response.use(
   },
   (error) => {
     if (error.response && error.response.status === 401) {
-      // Clear the expired token and redirect to the login page
-      localStorage.removeItem('token');
-      window.location.href = '/login'; // Adjust the path to your login route if different
+      const isLoginRequest =
+        error.config.url.includes('/api/Customer/Login') ||
+        error.config.url.includes('/api/Customer/AdminLogin');
+      console.log("heh",isLoginRequest)
+      // For non-login requests, handle token expiration
+      if (!isLoginRequest) {
+        // Clear the expired token and redirect to the login page
+        localStorage.removeItem('token');
+        window.location.href = '/login'; // Adjust the path to your login route if different
+      }
+
+      // For login requests, do not redirect or refresh
+      return Promise.reject(error);
     }
     return Promise.reject(error);
   }
 );
 
 export default axios;
-
